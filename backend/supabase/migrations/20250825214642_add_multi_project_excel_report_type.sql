599544e283dbb77a8d59ccc90bb181d1518ff14f
brianft <brian.f.thomsen@gmail.com>
2025-10-30 22:17:20 -0600

    map multi-project excel to other in ddx to enable report upload into ddx for projects imported via excel spreadsheet

--- backend/supabase/migrations/20250825214642_add_multi_project_excel_report_type.sql
+++ backend/supabase/migrations/20250825214642_add_multi_project_excel_report_type.sql
@@ -1,6 +1,6 @@
 -- Add multi-project Excel report type
 INSERT INTO "public"."enum_report_types" ("id", "created_at", "name", "order", "identifier_name", "file_type", "ddx_report_type_name") VALUES
-	(9, NOW(), 'Multi-Project Excel', NULL, 'multi_project_xlsx', '.xlsx', 'Multi-Project Excel')
+	(9, NOW(), 'Multi-Project Excel', NULL, 'multi_project_xlsx', '.xlsx', 'Other')
 ON CONFLICT (id) DO UPDATE SET
 	created_at = EXCLUDED.created_at,
 	name = EXCLUDED.name,
